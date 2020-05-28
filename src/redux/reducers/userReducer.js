83e8d7aec48965d7861b455102739dd1db4420ac
iamjink <jinijinjin@gmail.com>
2020-05-27 17:11:19 -0700

    moving home.js functions to redux in dataReducer and dataActions

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -2,7 +2,9 @@ import {
     SET_USER,
     SET_AUTHENTICATED,
     SET_UNAUTHENTICATED,
-    LOADING_USER
+    LOADING_USER,
+    LIKE_SCREAM,
+    UNLIKE_SCREAM
 } from '../types';
 
 //state from store
@@ -27,16 +29,32 @@ export default function (state = initialState, action) {
         case SET_USER:
             return {
                 authenticated: true,
-                loading: false,
-                //binds actions to state
+                    loading: false,
+                    //binds actions to state
                     ...action.payload
             };
         case LOADING_USER:
-            return{
+            return {
                 ...state,
                 loading: true
+            };
+        case LIKE_SCREAM:
+            return {
+                ...state,
+                likes: [
+                    ...state.likes,
+                    {
+                        userHandle: state.credentials.handle,
+                        screamId: action.payload.screamId
+                    }
+                ]
             }
-        default:
-            return state;
+            case UNLIKE_SCREAM:
+                return {
+                    ...state,
+                    likes: state.likes.filter(like => like.screamId === action.payload.screamId)
+                };
+                default:
+                    return state;
     }
 }
